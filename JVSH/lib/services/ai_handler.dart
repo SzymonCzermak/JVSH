b03a220a916796d224b03ea419cc2e3f3eb194ed
Szymon Czermak <SzyCzermak00@gmail.com>
2024-03-06 18:09:19 +0100

    TTS

--- JVSH/lib/services/ai_handler.dart
+++ JVSH/lib/services/ai_handler.dart
@@ -1,13 +1,17 @@
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:gpt_flutter/services/tts_handler.dart';
 
 class AIHandler {
   final _openAI = OpenAI.instance.build(
-    token: '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',
+    token:
+        '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', // Zastąp 'twoj-klucz-api' swoim faktycznym kluczem API
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
     ),
   );
+
+  final TtsHandler _ttsHandler = TtsHandler(); // Dodanie instancji TtsHandler
 
   Future<String> getResponse(String message) async {
     try {
@@ -16,11 +20,14 @@ class AIHandler {
       ], maxToken: 200, model: kChatGptTurbo0301Model);
 
       final response = await _openAI.onChatCompletion(request: request);
-      if (response != null) {
-        return response.choices[0].message.content.trim();
+      if (response != null && response.choices.isNotEmpty) {
+        String textResponse = response.choices[0].message.content.trim();
+        _ttsHandler
+            .speak(textResponse); // Wywołanie TTS do odczytania odpowiedzi
+        return textResponse;
       }
 
-      return 'Some thing went wrong';
+      return 'Something went wrong';
     } catch (e) {
       return 'Bad response';
     }
@@ -28,5 +35,6 @@ class AIHandler {
 
   void dispose() {
     _openAI.close();
+    _ttsHandler.stop(); // Zatrzymaj TTS przy zamykaniu
   }
 }
---
+++ JVSH/lib/services/tts_handler.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TtsHandler {
+  final FlutterTts _flutterTts = FlutterTts();
+
+  TtsHandler() {
+    _flutterTts.setLanguage("pl-PL");
+    _flutterTts.setSpeechRate(0.5);
+    _flutterTts.setVolume(1.0);
+    _flutterTts.setPitch(5.0);
+  }
+
+  Future speak(String message) async {
+    if (message.isNotEmpty) {
+      await _flutterTts.speak(message);
+    }
+  }
+
+  void stop() {
+    _flutterTts.stop();
+  }
+}
